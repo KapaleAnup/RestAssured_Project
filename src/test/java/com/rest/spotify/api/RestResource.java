252bd72507e1bc2129012711c5a4a551f0ab4c69
Anup kapale <anup.kapale@gmail.com>
2021-04-25 20:08:51 +0530

    Added Rest Sourcess class as a common utility

---
+++ src/test/java/com/rest/spotify/api/RestResource.java
@@ -0,0 +1,41 @@
+package com.rest.spotify.api;
+
+import com.rest.spotify.pojo.PlayList;
+import io.restassured.response.Response;
+
+import static com.rest.spotify.api.SpecBuilder.getRequestSpec;
+import static com.rest.spotify.api.SpecBuilder.getResponseSpec;
+import static io.restassured.RestAssured.given;
+
+public class RestResource {
+   // static String access_token ="BQDXLBspsOvtJ9IRhT6UDEpFCLy6hbxbjoe1_5xEQ8ZC6fU8e0qpOEXxRt8QfM7Ea_UPnWjllIF2uui0ah_JJfKL5lbCInohmux7q10KiOlumY3evVUM_LvPCPeHBJFP1oDbIas95_i9JiRGOSPnsk_Z-tzVwReCk6QpLnwtPAw_piyYGj9esnYICUdKJnvJsJonqUzA2PGB0MyLVae1xsXilDmcW49Yxm9s1_EB1Sc3";
+
+    public static Response post(String path, String token , Object requestPayload){
+       return  given(getRequestSpec())
+                .body ( requestPayload )
+                .header ( "Authorization","Bearer "+token )
+                .when().post (path)
+                .then ().spec ( getResponseSpec () )
+                .extract ()
+                .response ();
+    }
+
+    public static Response get(String path, String token){
+   return given(getRequestSpec ())
+                .header ( "Authorization","Bearer "+ token )
+                .when().get(path)
+                .then ().spec ( getResponseSpec () )
+                .extract ()
+                .response ();
+    }
+
+    public static Response update ( String path, String token , Object requestPayload){
+        return given(getRequestSpec ())
+                .body ( requestPayload )
+                .header ( "Authorization","Bearer "+ token )
+                .when().put(path)
+                .then ().spec ( getResponseSpec () )
+                .extract ()
+                .response ();
+    }
+}
